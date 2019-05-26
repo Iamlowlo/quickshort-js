58545e4f1a0ac3d9d541d37dc9d419e0b604a6fd
IamLoWLo <iamlowlo@gmail.com>
2019-05-26 20:35:05 +0200

    [QS1+2]: Added method including random pivot and two groups reducer. Added method Medium alike (not working)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export const sort = (origin) => {
+const [node, sourcePath, origin, compare, ...argv] = process.argv;
 
-};
+console.log('origin', origin);
+console.log('compare', compare);
+console.log('argv', argv);
